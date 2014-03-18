0d13abd26692643bc5f16fae4c088fa9a432b30a
palkeo <contact@palkeo.com>
2014-03-18 19:17:15 +0100

    IA qui fonctionne (limite de temps, pas de limite de taille à la liste des trucs à tester)

--- codingame.py
+++ codingame.py
@@ -1,7 +1,13 @@
 import math
 import sys
+import operator
+import datetime
 
 WAR_WEIGHT_TAU = 0.99
+ZONE_SIZE = 100
+DRONE_SPEED = 100
+
+TW = [1]*60
 
 class Point(object):
     def __init__(self, x=0, y=0):
@@ -18,53 +24,90 @@ class Zone(Point):
     def __init__(self, game, team, *args, **kwargs):
         self.team = team
         self.game = game
-        self.war_weight = 1
+        self._war_weight = 1
+        self.turn = 0
         super().__init__(*args, **kwargs)
 
+    def __repr__(self):
+        return 'Zone(%s, %s)' % (self.x, self.y)
+
     def drones_inside(self):
-        yield from filter(lambda drone: self.distance(drone) <= 100, self.game.drones)
+        yield from filter(lambda drone: self.distance(drone) <= ZONE_SIZE, self.game.drones)
 
-    def drones_needed(self):
-        by_team = [0 for _ in self.game.teams]
-        for drone in self.game.drones:
-            if drone.team == self.game.my_team:
-                continue
+    def drones_needed(self, drones):
+        """ Return a list of (score, drone_list), for each possible solution """
+        turns_remaining = 201 - self.turn
+        drones = sorted(drones, key=lambda drone: drone.distance(self))
+        s = []
 
-            if drone.zone_objective() == self:
-                by_team[drone.team] += 1
+        for nb_drones in range(1, len(drones) + 1):
+            my_drones = drones[:nb_drones]
+            my_drones_count = 0
+            foe_drones_count = [0 for _ in self.game.teams]
+            foe_drones = sorted(filter(lambda d: d.team != self.game.my_team and d.zone_objective() == self, self.game.drones), key=lambda d: d.distance(self))
+            score = 0
+            is_mine = self.team == self.game.my_team
 
-        return max(by_team) + int(self.team != self.game.my_team)
+            for turns, weight in enumerate(TW[:turns_remaining]):
+                distance = (turns + 1) * DRONE_SPEED + ZONE_SIZE
+                while my_drones and my_drones[0].distance(self) <= distance:
+                    my_drones_count += 1
+                    my_drones.pop(0)
+                while foe_drones and foe_drones[0].distance(self) <= distance:
+                    foe_drones_count[foe_drones.pop(0).team] += 1
+
+                fc = max(foe_drones_count)
+                is_mine = my_drones_count > fc or (my_drones_count == fc and is_mine)
+                score += weight * int(is_mine)
+
+            # TODO: Add an infinite bidule
+
+            if score > 0:
+                s.append((score, drones[:nb_drones]))
+
+        return s
       
     def update(self):
-        self.turns += 1
-
+        self.turn += 1
         by_team = [0 for _ in self.game.teams]
         for drone in self.game.drones:
             if drone.zone_inside() == self:
                 by_team[drone.team] += 1
 
-        self.war_weight = WAR_WEIGHT_TAU*self.war_weight + (1 - WAR_WEIGHT_TAU)*max(by_team)
+        self._war_weight = WAR_WEIGHT_TAU*self._war_weight + (1 - WAR_WEIGHT_TAU)*max(by_team)**2
+
+    @property
+    def war_weight(self):
+        return math.sqrt(self._war_weight)
 
 class Drone(Zone):
     def __init__(self, drone_id, *args, **kwargs):
         self.drone_id = drone_id
         self.old_x, self.old_y = None, None
+        self._zone_objective = None
         super().__init__(*args, **kwargs)
+
+    def __repr__(self):
+        return 'Drone(%s, %s)' % (self.x, self.y)
     
     def update(self, x, y):
         self.old_x, self.old_y = self.x or x, self.y or y
         self.x, self.y = x, y
+        self._zone_objective = None
     
     def direction(self):
         return Point(self.x - self.old_x, self.y - self.old_y)
 
     def zone_inside(self):
         for z in self.game.zones:
-            if self.distance(z) <= 100:
+            if self.distance(z) <= ZONE_SIZE:
                 return z
         return None
     
     def zone_objective(self):
+        if self._zone_objective is not None:
+            return self._zone_objective
+        
         direction = self.direction()
         if direction.x == 0 and direction.y == 0:
             return self.zone_inside()
@@ -73,11 +116,13 @@ class Drone(Zone):
         min_z = None
         for z in self.game.zones:
             p = self.projection(z)
-            if p.distance(z) <= 100:
+            if p.distance(z) <= ZONE_SIZE:
                 d = self.distance(p)
                 if min_d is None or d < min_d:
                     min_d = d
                     min_z = z
+
+        self._zone_objective = min_z
         return min_z
         
     def projection(self, zone):
@@ -147,58 +192,49 @@ class Game(object):
 
     def play(self):
         self.turn += 1
+        self.turn_start = datetime.datetime.now()
 
         default_zone = None
         for z in self.zones:
             if default_zone is None or z.war_weight > default_zone.war_weight:
                 default_zone = z
 
-        vascript
-            
-            03/17 02:31:06
-                
-                    
-                    44,50
-                    11
-                        
-                        S010|
-        commands = [default_zone for _ in self.teams[self.my_team]]
+        best_action, best_score = self.recurse(self.zones, self.teams[self.my_team])
 
-        zones = set(self.zones)
-        drones = set(self.teams[self.my_team])
-        while drones:
-            best_score = None
-            best_action = None
-            
-            for zone in zones:
-                needed = zone.drones_needed()
-                if len(drones) < needed:
-                    continue
-                
-                nearest_drones = sorted(drones, key=lambda drone: drone.distance(zone))[:needed]
-                
-                weight = (max(drone.distance(zone) for drone in nearest_drones) if nearest_drones else 0)
-                weight *= zone.war_weight
-                
-                score = (needed, weight)
-                
-                if best_score is None or score < best_score:
-                    best_score = score
-                    best_action = (nearest_drones, zone)
-            
-            if best_action is None:
-                print("WTF", file=sys.stderr)
-                break
-            
-            d, zone = best_action
-            zones.remove(zone)
-            for i in d:
-                commands[i.drone_id] = zone
-            drones.difference_update(d)
+        for drone in self.teams[self.my_team]:
+            try:
+                zone = best_action[drone.drone_id]
+            except KeyError:
+                print("[Warning] Drone %s without any command." % drone, file=sys.stderr)
+                zone = default_zone
+            print("%s %s" % (zone.x, zone.y))
+    
+    def recurse(self, zones, drones):
+        if not zones or not drones or (datetime.datetime.now() - self.turn_start).total_seconds() > 0.09:
+            return {}, 0
 
-        for command in commands:
-            print("%s %s" % (command.x, command.y))
+        drones = set(drones)
+        zones = set(zones)
+        h = []
 
+        for zone in zones:
+            for score, needed in zone.drones_needed(drones):
+                rscore = score / len(needed)
+                h.append((rscore, score, needed, zone))
 
+        #print(len(zones), len(drones), sorted(h, key=operator.itemgetter(0), reverse=True), file=sys.stderr)
+
+        best_score = 0
+        best_action = {}
+        for _, score, needed, zone in sorted(h, key=operator.itemgetter(0), reverse=True):
+            action, subscore = self.recurse(zones.difference((zone,)), drones.difference(needed))
+            subscore += score
+            if subscore > best_score:
+                best_score = subscore
+                best_action = action
+                best_action.update((i.drone_id, zone) for i in needed)
+
+        return best_action, best_score
+                
 g = Game()
 g.run()
